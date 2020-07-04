9f3d7bd33ec2efa2511893ccadf20995edb5e160
ckt371461 <ckt371461@gmail.com>
2020-07-04 17:51:59 +0800

    modify gradient_descent1.py

--- ai/gradient_descent1.py
+++ ai/gradient_descent1.py
@@ -3,12 +3,6 @@ def kadd(a,i,c):#  a[i] = a[i]+c
     r[i] += c
     return r
 
-def grad(f, p, h=0.00001): #偏微分,f是函數,p是[x,y,z...]的值
-    end = p.copy()
-    for i in range(len(p)):
-        end[i] = (f(kadd(p,i,h))- f(p))/h
-    return end
-
 def length(p):#向量的長度,p是[x,y,z...]的值
     sum=0
     for x in p:
@@ -20,10 +14,16 @@ def vmul(v,c):#v是p偏微分後的結果(=g)
         v[x] = v[x]*c
     return v
 
-def vadd(p,v):
+def vadd(p,v):n
     for x in range(len(p)):
         p[x] += v[x]
     return p
+
+def grad(f, p, h=0.00001): #偏微分,f是函數,p是[x,y,z...]的值
+    end = p.copy()
+    for i in range(len(p)):
+        end[i] = (f(kadd(p,i,h))- f(p))/h
+    return end
 
 def gradient_descent(f,p0):#梯度下降,f是函數,p0是起點
     p = p0.copy()
